77c413c7bec6648f84fbd78e1d5759f901cc79d1
tnt.galicia@gmail.com <tnt.galicia@gmail.com@8a0d2a07-0b69-808f-86d7-54b5bdfc3ce0>
2015-01-31 09:03:43 +0000

    -Add meta description -delete css -add tag alt to images -compress camera.css -block libwww-perl under Apache web server

--- trunk/index.php
+++ trunk/index.php
@@ -9,11 +9,11 @@
 <meta http-equiv="expires" content="0" />
 <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
 <meta http-equiv="pragma" content="no-cache" />
-    
+<meta name="description" content="Grupo orientado a capacitación de los profesionales de la medicina crítica">    
 	<link rel="stylesheet" type="text/css" media="screen" href="css/animate.css">
-	
-    <link rel='stylesheet' id='camera-css'  href='css/camera.css' type='text/css' media='all'> 
+	<link rel='stylesheet' id='camera-css'  href='css/camera.css' type='text/css' media='all'> 
     <link href='http://fonts.googleapis.com/css?family=Condiment' rel='stylesheet' type='text/css'>
+	
     <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
     <script src="js/jquery-1.7.min.js"></script>
     <script src="js/jquery.easing.1.3.js"></script>
@@ -24,16 +24,7 @@
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
-	<style>
-	.logotipos_asociados {
-	background:oldlace;
-	border: solid 1px silver;
-	max-height: 320px;
-	padding: 10px;
-	
-}
-</style>
-	
+
     <script>
 		jQuery(function(){
 			jQuery('#camera_random').camera({
@@ -51,13 +42,13 @@
 <div class="container main">
 <div  class="row animated zoomIn " id="logo">
 	<div class="col-md-3 col-md-offset-1" style="margin-left: 4%;">
-		<img class="img-responsive" src="images/Imagen1.png" alt="" style="height:100px">
+		<img class="img-responsive" src="images/Imagen1.png" alt="Nosotros" style="height:100px">
 	</div>
 	<div class="col-md-3" style="margin-left: 13%;width: 16% !important;">
-		<img class="img-responsive" src="images/FCCS.png" alt="" style="margin-top: 20px;height:75px;">
+		<img class="img-responsive" src="images/FCCS.png" alt="Un equipo" style="margin-top: 20px;height:75px;">
 	</div>
 	<div class="col-md-4" style="margin-left: 11%;width: 29% !important;">
-		<img class="img-responsive" src="images/aha_logo4.png" alt="" style="margin-top: 30px;height:65px">
+		<img class="img-responsive" src="images/aha_logo4.png" alt="Un grupo" style="margin-top: 30px;height:65px">
 	</div>
 </div>
 
@@ -97,6 +88,7 @@
  </div>
  </div>
 <div id="contenido" class="animated fadeInRight row" style="min-height:400px;"> 
+		<div class="row">
 		<div class="col-md-7" >
 			 <div class="fluid_container">
 				 <div class="camera_wrap camera_charcoal_skin" id="camera_random">
@@ -120,10 +112,12 @@
 		</div>
 		
 		
-		<div class="logotipos_asociados col-md-5">
+		<div class="col-md-5 col-sm-2">
 		<img class="img-responsive img-thumbnail  center-block" src="images/Angeles Lomas.gif" alt="Random image" style="height:160px;" />
 		<h5 id="Lomas"><button type="button" class="btn btn-info center-block">Inscripción</button></h5>
 		</div>
+		</div>
+		
 </div>
 </div>	
 
